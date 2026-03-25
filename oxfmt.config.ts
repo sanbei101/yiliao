12da3b8ff2b397e11f9a800774bd01d3b51e82ab
sanbei101 <sanbei101@outlook.com>
2026-03-25 18:33:06 +0800

    init

---
+++ oxfmt.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "oxfmt";
+
+export default defineConfig({
+  printWidth: 100,
+  sortTailwindcss: {
+    functions: ["clsx", "cn"],
+    stylesheet: "src/style.css",
+  },
+  ignorePatterns: ["src/components/ui"],
+});
