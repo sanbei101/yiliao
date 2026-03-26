731b39c544ac5b7662d1dee69ef3455486910db0
sanbei101 <sanbei101@outlook.com>
2026-03-26 22:10:51 +0800

    优化页面结构，添加路由过渡效果

--- src/App.vue
+++ src/App.vue
@@ -36,6 +36,22 @@ function goToHome() {
       </div>
     </header>
 
-    <RouterView />
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </div>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
